9f60e469b903dc8b82df6daa4af5a78c6eb2da88
Milena <milenacipriani.mail@gmail.com>
2025-02-21 08:02:06 +0100

    Formatting corrections + added react frontend connected to api service

--- api-node/src/index.js
+++ api-node/src/index.js
@@ -1,7 +1,10 @@
+const cors = require('cors'); //allow cross-origin requests from frontend
 const { getDateTime } = require('./db');
 const express = require('express') //Dockerfile will install this dependency with npm install, reading package.json and package-lock.json
 const app = express()
 const port = process.env.PORT || 3000;
+
+app.use(cors()); // enable CORS for all requests
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
@@ -9,11 +12,11 @@ app.listen(port, () => {
 
 //HOME ****
 app.get('/', async (req, res) => {
-    const dateTime = await getDateTime();
-    const response = dateTime;
-    response.api = 'node';
-    res.send(response);
-  });
+  const dateTime = await getDateTime();
+  const response = dateTime;
+  res.send(response);
+  //res.json(response); // Send response as JSON cause react doesn't like object with keys
+});
 
 //PING ****
 app.get('/ping', async (_, res) => {
---
+++ react-client/src/App.jsx
@@ -0,0 +1,50 @@
+import { useState, useEffect } from 'react' //useEffect for API call
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import axios from 'axios' //for HTTP requests
+
+function App() {
+  const [data, setData] = useState(null); // State to store API response
+  const [count, setCount] = useState(0)
+
+  // Fetch data from the API when the component loads
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000")
+      .then((response) => setData(response.data)) // Set response data into state
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []); // Empty dependency array means this runs once when the component mounts
+
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          Click count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+
+      {/* Display API response */}
+      <h2>API Test</h2>
+      {data ? <p>API Response: {JSON.stringify(data)}</p> : <p>Loading...</p>}
+    </>
+  )
+}
+
+export default App
